6f9d155f16254b8d8efcdc466905faca62862ca2
MOOMIN\music <music378world@gmail.com>
2025-06-04 00:28:27 +0900

    《post》ブレードの css　を設定しました。

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,26 +1,29 @@
 <x-login-layout>
 
 
-  <h2>機能を実装していきましょう。</h2>
-
-  <div class="container">
-    <img src=" images/{{Auth::user()->icon_image }}">
+  <!-- <h2>機能を実装していきましょう。</h2> -->
+<div class="post">
+    <img class="post_icon" src=" images/{{Auth::user()->icon_image }}">
     {{ Form::open(['url' => '/post_create']) }}
     @csrf
-    {{ Form::text('post',null,['required', 'class' => 'texts', 'placeholder' => '投稿内容を入力してください。']) }}
+    {{ Form::textarea('post',null,['required', 'class' => 'texts', 'placeholder' => '投稿内容を入力してください。']) }}
 
-      <input type="image" name="submit" src="{{asset('images/post.png')}}" alt="送信" class="btn">
+      <input type="image" name="submit" src="{{asset('images/post.png')}}" alt="送信" class="post_btn">
       {{ Form::close() }}
 
-  </div>
-  <div class="container">
-    <!-- フォローしているメンバーの投稿を新しい順番で表示 -->
 
 
 
-  </div>
 
 
+
+</div>
+<hr>
+<div>
+      フォローしているメンバーの投稿を新しい順番で表示
+
+
+</div>
 
 
 </x-login-layout>
