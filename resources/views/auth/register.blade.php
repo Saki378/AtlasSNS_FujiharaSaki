1f62083624bfd60d140f002289acc0b990b140aa
MOOMIN\music <music378world@gmail.com>
2025-06-30 15:46:12 +0900

    no message

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,37 +2,38 @@
     <!-- 適切なURLを入力してください -->
 {!! Form::open(['url' => 'register']) !!}
 
-<h2>新規ユーザー登録</h2>
+<div class="register">
+    <h2>新規ユーザー登録</h2>
 
-{{ Form::label('ユーザー名') }}
-{{ Form::text('username',null,['class' => 'input']) }}
+    {{ Form::label('ユーザー名') }}
+    {{ Form::text('username',null,['class' => 'input']) }}
 
-{{ Form::label('メールアドレス') }}
-{{ Form::email('email',null,['class' => 'input']) }}
+    {{ Form::label('メールアドレス') }}
+    {{ Form::email('email',null,['class' => 'input']) }}
 
-{{ Form::label('パスワード') }}
-{{ Form::password('password',null,['class' => 'input']) }}
+    {{ Form::label('パスワード') }}
+    {{ Form::password('password',null,['class' => 'input']) }}
 
-{{ Form::label('パスワード確認') }}
-{{ Form::password('password_confirmation',null,['class' => 'input']) }}
+    {{ Form::label('パスワード確認') }}
+    {{ Form::password('password_confirmation',null,['class' => 'input']) }}
 
-<!-- エラーメッセージ -->
-@error('username')
-<p>{{ $message}}</p>
-@enderror
+    <!-- エラーメッセージ -->
+    @error('username')
+    <p>{{ $message}}</p>
+    @enderror
 
-@error('email')
-<p>{{ $message}}</p>
-@enderror
+    @error('email')
+    <p>{{ $message}}</p>
+    @enderror
 
-@error('password')
-<p>{{ $message}}</p>
-@enderror
+    @error('password')
+    <p>{{ $message}}</p>
+    @enderror
 
-{{ Form::submit('新規登録') }}
+    {{ Form::submit('新規登録') }}
 
-<p><a href="login">ログイン画面へ戻る</a></p>
-
+    <p><a href="login">ログイン画面へ戻る</a></p>
+</div>
 {!! Form::close() !!}
 
 
